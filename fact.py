a322a4004cea76b78d89123987f7638a3cd9823c
VishweshRathi <vishweshsr@gmail.com>
2017-01-09 17:11:47 +0530

    1. Program for finding the Factorial of the Number.

---
+++ fact.py
@@ -0,0 +1,18 @@
+"""
+This is First Code in python.
+"""
+
+
+__author__= 'Vishwesh'
+
+def factorial():
+	fact=1
+ 	num = input("Please Enter the number: ")
+	
+	while num>0:
+		fact = fact * num
+		num = num - 1
+	print fact
+	
+if __name__== "__main__":
+	factorial()
