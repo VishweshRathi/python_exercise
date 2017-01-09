1c2b61a205d66c2b9def8f9c46314d92d1c9601d
VishweshRathi <vishweshsr@gmail.com>
2017-01-09 18:00:04 +0530

    2.Removed Excess/unused code from file.

--- panvalidate.py
+++ panvalidate.py
@@ -1,17 +1,12 @@
 """
-This is First Code in python.
+Code for PAN Validation
 """
 
 import datetime
 __author__= 'Vishwesh'
 
 def panvalidate():
-	
-	aa=datetime.date(2026,12,11)
-	aa+=datetime.timedelta(hours=4)
-	ab=datetime.datetime.combine(aa,datetime.time(5))	
-	print ab
-	
+
 #------------------------------------------------------------------------------------------
 	Pan1= raw_input("Please Enter PAN Number: ")
 	Pan= Pan1.upper()
